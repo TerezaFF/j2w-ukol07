e52bfdb0f96cd2e12b51279bd21da2ba9e466fba
Tereza Faustova <tereza.faustova@capgemini.com>
2021-05-25 08:20:03 +0200

    Ukol 7 oprava

--- src/main/java/cz/czechitas/java2webapps/ukol7/entity/Vizitka.java
+++ src/main/java/cz/czechitas/java2webapps/ukol7/entity/Vizitka.java
@@ -44,25 +44,6 @@ public class Vizitka {
     @Length(max = 100)
     private String web;
 
-    @Transient
-    private String celaAdresa;
-
-    public Vizitka(Integer id, String celeJmeno, String firma, String ulice, String obec, String psc, String email, String telefon, String web) {
-        this.id = id;
-        this.celeJmeno = celeJmeno;
-        this.firma = firma;
-        this.ulice = ulice;
-        this.obec = obec;
-        this.psc = psc;
-        this.email = email;
-        this.telefon = telefon;
-        this.web = web;
-    }
-
-    public Vizitka() {
-
-    }
-
     public void setId(Integer id) {
         this.id = id;
     }
